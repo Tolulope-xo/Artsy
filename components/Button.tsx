8abbb27bc4451827e0c35de207448d2b81f10847
Tolulope_xo <60390674+Tolulope-xo@users.noreply.github.com>
2023-03-30 13:50:35 +0000

    Added the prop type

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-
-const Button = ({ text }) => {
-  return (
+interface Props {
+  text: string;
+}
+const Button: React.FC<Props> = ({ text }) => {
+    return (
     <div>
       <button type="submit" className='bg-[#3341C1] w-[20rem] font-[500] text-[25px] font-[Satoshi] text-[#ffffff] h-[4rem]'>{text}</button>
     </div>
